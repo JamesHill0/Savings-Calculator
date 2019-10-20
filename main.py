93401f615119fe1822774ef125c8c1c70556110e
Aaron Lebel <alebel@aaron-mbp.wv.cc.cmu.edu>
2019-10-20 15:09:52 -0400

    updated max year functionality

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 #IMPORTS
 #################################################################################
 from flask import *
+from test import *
 
 #################################################################################
 #GLOBALS
@@ -14,28 +15,22 @@ app = Flask(__name__)
 #FLASK ROUTING
 #################################################################################
 
-@app.route('/', methods = ["POST"])
+@app.route('/', methods = ["GET"])
 def index():
-    if request.form["authenticator"] == "test":
-        authentication_status = True
-    else:
-        authentication_status = False
+    return render_template("index.html")
 
-    output_params = {
-        "authentication_status" : authentication_status
-    }
-
-    return jsonify(output_params)
-
-@app.route('/series_data', methods = ["POST"])
+@app.route('/series_data', methods = ["POST", "GET"])
 def get_series_data():
-    if request.form["authenticator"] == "test":
-        authentication_status = True
-    else:
-        authentication_status = False
+    authentication_status = True
+    start = int(request.args['start'])
+    end = int(request.args['end'])
+    initial_contrib = int(request.args['initial_contrib'])
+    annual_contrib = int(request.args['annual_contrib'])
+    output = cumulativeReturnRange(start, end, initial_contrib, annual_contrib)
 
     output_params = {
-        "authentication_status" : authentication_status
+        "authentication_status" : authentication_status,
+        "output" : output
     }
 
     return jsonify(output_params)
--- test.py
+++ test.py
@@ -26,21 +26,26 @@ def cumulativeReturn(years_ago, initial_contribution, data):
         print("\n"+str(row)+"\n")
         return int(round(initial_contribution * float(row['c_return'])))
 
-def cumulativeReturnRange(start_bound, end_bound, initial_contribution, annual_contribution, data):
+def getSampleSpace(start_bound, end_bound):
+    years_ago_start = 2019 - int(start_bound)
+    years_ago_end = 2019 - int(end_bound)
+    space = []
+    
+    for row in range(years_ago_end - 1, years_ago_start):
+        space.append(row)
+
+    return reversed(space)
+    
+
+def cumulativeReturnRange(start_bound, end_bound, initial_contribution, annual_contribution):
     if int(end_bound) > 2019 or int(start_bound) < 1926 or (int(end_bound) - int(start_bound)) < 0:
         return "Invalid Range"
     else:
-        years_ago_start = 2019 - int(start_bound)
-        years_ago_end = 2019 - int(end_bound)
         balance = initial_contribution
+        data = list_of_hashes
+        space = getSampleSpace(start_bound, end_bound)
         running = 0
-        space = []
-        
-        for row in range(years_ago_end - 1, years_ago_start):
-            space.append(row)
 
-        space = reversed(space)
-        
         for row in space:
             print("YEAR : %s" %(data[row]['year']))
             print("Return Muliplier: %s" %((1 + data[row]['a_return'])/ 100))
@@ -50,6 +55,22 @@ def cumulativeReturnRange(start_bound, end_bound, initial_contribution, annual_c
             balance = ((1 + float(data[row]['a_return'])/100) * balance) + annual_contribution * ((1 + float(data[row]['a_return'])/ 100))
 
         return int(round(balance))
+
+def maxYearReturn(start_bound, end_bound):
+    data = list_of_hashes
+    space = getSampleSpace(start_bound, end_bound)
+    year_storage = None
+    percent_storage = None
+
+    for row in space:
+        print("ROW : %s" %(data[row]))
+        if percent_storage == None or float(data[row]['a_return']) > percent_storage:
+            percent_storage = float(data[row]['a_return'])
+            year_storage = int(data[row]['year'])
+        
+    return (year_storage, percent_storage)
+
+        
 
 
 def testCumulativeReturn():
@@ -66,23 +87,29 @@ def testCumulativeReturn():
     print("--")
 
 def testCumulativeReturnRange():
-    start_bound = 2000
+    start_bound = 1950
     end_bound = 2018
     initial_c = 1000
     annual_c = 100
-    data = list_of_hashes
     print()
     print("Function: testCumulativeReturnRange")
     print("--")
     print("Range: %i, %i" %(start_bound, end_bound))
     print("Initial Contribution: %i" %(initial_c))
-    print("Annual Contribution: %i" %(annual_c))
     print("--")
-    print(cumulativeReturnRange(start_bound, end_bound, initial_c, annual_c, data))
+    print(cumulativeReturnRange(start_bound, end_bound, initial_c, annual_c))
     print("--")
 
+def testMaxYearReturn():
+    start_bound = 1950
+    end_bound = 2018
+    print(maxYearReturn(start_bound, end_bound))
+
+
 def main():
-    testCumulativeReturnRange()
+    #testCumulativeReturn()
+    #testCumulativeReturnRange()
+    testMaxYearReturn()
 
 if __name__ == "__main__":
     main()
