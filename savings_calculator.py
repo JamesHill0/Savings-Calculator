1a7223ca6c6cce578241703c277b9b48d5ddb63a
Aaron Lebel <aaron@pairi.app>
2020-01-11 19:43:34 -0500

    update

--- savings_calculator.py
+++ savings_calculator.py
@@ -1,5 +1,39 @@
-def main():
-    pass
+import csv
+
+
+
+"""
+
+    Get the corresponding row given occupation and location
+
+"""
+def getRow(occupation, location):
+    with open('savings_calculator.csv', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+
+        for line in csv_reader:
+            if line[1] == location and line[9] == occupation:
+                print(line)
+                return line
+            else:
+                pass
+        
+        return None
+
+"""
+
+Test CSV load speed to ensure efficient page load times
+
+"""        
+def speedTest():
+    with open('savings_calculator.csv', 'r') as csv_file:
+        csv_object = csv.reader(csv_file)
+
+        for line in csv_object:
+            print(line)
+
+def main():   
+    print(getRow("Occupational Therapists", "Connecticut"))
 
 if __name__ == "__main__":
     main()
